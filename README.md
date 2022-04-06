3ee298bd0b44e80031411cc8d7ff61a1fba3bae9
Saryn <halecone@gmail.com>
2022-04-07 02:22:44 +0800

    2. Vue通过axios访问Flask RESTful API

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # SarynBlog
-vue、flask
+
+本项目采用 Vue2、FLask、element-UI 搭建
+
+# 1.1 如何使用
+
+环境 python3.10，npm6.14，nodejs8.11.2，vue2.9.6
+
+## 1.2 Backend
+
+打开终端:
+
+```bash
+$ cd back-end
+$ python -m venv venv
+$ venv/Scripts/activate.act
+(venv)$ pip install -r requirements.txt
+
+# 创建更新数据库
+(venv)$ flask db upgrade
+
+# 创建.env文件并输入
+FLASK_APP=madblog.py
+FLASK_DEBUG=1
+
+(venv)$ flask run
+```
+
+浏览器访问: `http://localhost:5000/api/ping`
+
+## 2.2 Frontend
+
+打开终端:
+
+```bash
+$ cd front-end
+$ npm install
+$ npm run dev
+```
+
+浏览器访问: `http://localhost:8080`
---
+++ front-end/src/components/Ping.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <el-button type="primary">{{msg}}</el-button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Ping',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    getMessage () {
+      const path = 'http://127.0.0.1:5000/api/ping'
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        })
+    }
+  },
+  created () {
+    this.getMessage()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
